91b92dde1dc96155c1de29f4f542c79fac806489
mikplavsky <mikhail.plavskiy@quest.com>
2020-10-31 23:37:22 +0300

    added Pool

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 from flask import Flask
+from gevent.pywsgi import WSGIServer 
+
+from gevent import monkey, sleep
+from gevent.pool import Pool
+
+monkey.patch_all()
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
 
-    import time
-    time.sleep(1)
-
+    sleep(1)
     return "Done!\n"
+
+wsgi = WSGIServer(
+        ('0.0.0.0',5000), 
+        app, 
+        spawn=Pool(1000))
+
+wsgi.serve_forever()
